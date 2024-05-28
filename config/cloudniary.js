1285265bb554fd5db1b575729deef14722f5ead3
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-05-29 00:32:58 +0530

    commit

--- config/cloudniary.js
+++ config/cloudniary.js
@@ -1,14 +1,26 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2; // Use the v2 of the cloudinary API
 require("dotenv").config();
 
-exports.cloudinary = () => {
+exports.cloudinaryConnect = () => {
   try {
+    if (
+      !process.env.CLOUDINARY_NAME ||
+      !process.env.CLOUDINARY_API_KEY ||
+      !process.env.CLOUDINARY_API_SECRET
+    ) {
+      throw new Error(
+        "Missing Cloudinary configuration in environment variables"
+      );
+    }
+
+    console.log("Connecting to Cloudinary...");
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
+    console.log("Cloudinary connection successful.");
   } catch (error) {
-    console.log(error);
+    console.error("Error connecting to Cloudinary:", error.message);
   }
 };
